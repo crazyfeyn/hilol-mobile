3a12ee7dcc10913d182424acf73debc15111323e
golibjon <golibtoramurodov@gmail.com>
2026-05-12 10:47:23 +0900

    jwhef

--- lib/data/repositories/product_repository_impl.dart
+++ lib/data/repositories/product_repository_impl.dart
@@ -6,6 +6,7 @@ import 'package:commerce_mobile/data/models/product_model.dart';
 import 'package:commerce_mobile/domain/repositories/product_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:commerce_mobile/core/utils/app_snackbar.dart';
+import 'package:flutter/material.dart';
 
 class ProductRepositoryImpl extends ProductRepository {
   late final CancelTokenManager cancelTokenManager;
@@ -23,10 +24,11 @@ class ProductRepositoryImpl extends ProductRepository {
       final response = await NetworkService.get(api, cancelToken);
 
       final data = response['data']['list'] as List;
-      final categories = data.map((json) => ProductCategoryModel.fromMap(json)).toList();
+      final categories =
+          data.map((json) => ProductCategoryModel.fromMap(json)).toList();
       return Right(categories);
-    } on NetworkException catch(e) {
-      if(e.type != NetworkExceptionType.cancelled) {
+    } on NetworkException catch (e) {
+      if (e.type != NetworkExceptionType.cancelled) {
         GlobalSnackBar.showError(e.message);
       }
       return Left(e.toString());
@@ -46,8 +48,8 @@ class ProductRepositoryImpl extends ProductRepository {
       final products = data.map((json) => ProductModel.fromMap(json)).toList();
 
       return Right(products);
-    } on NetworkException catch(e) {
-      if(e.type != NetworkExceptionType.cancelled) {
+    } on NetworkException catch (e) {
+      if (e.type != NetworkExceptionType.cancelled) {
         GlobalSnackBar.showError(e.message);
       }
       return Left(e.toString());
@@ -61,33 +63,40 @@ class ProductRepositoryImpl extends ProductRepository {
     try {
       final api = NetworkService.apiGetProductById;
       final cancelToken = cancelTokenManager.getToken(api);
-      final response = await NetworkService.get(api, cancelToken, {'productId': productId});
+      final response = await NetworkService.get(api, cancelToken, {
+        'productId': productId,
+      });
 
       final data = response['data'];
       return Right(ProductModel.fromMap(data));
-    } on NetworkException catch(e) {
-      if(e.type != NetworkExceptionType.cancelled) {
+    } on NetworkException catch (e) {
+      if (e.type != NetworkExceptionType.cancelled) {
         GlobalSnackBar.showError(e.message);
       }
       return Left(e.toString());
-    } catch (e) {
+    } catch (e, st) {
+      debugPrint('❌ fetchProductById: $e\n$st');
       return Left(e.toString());
     }
   }
 
   @override
-  Future<Either<String, List<ProductModel>>> fetchProductsByCategory(int categoryId) async {
+  Future<Either<String, List<ProductModel>>> fetchProductsByCategory(
+    int categoryId,
+  ) async {
     try {
       final api = NetworkService.apiGetProductsByCategory;
       final cancelToken = cancelTokenManager.getToken(api);
-      final response = await NetworkService.get(api, cancelToken, {'categoryId': categoryId});
+      final response = await NetworkService.get(api, cancelToken, {
+        'categoryId': categoryId,
+      });
 
       final data = response['data']['list'] as List;
       final products = data.map((json) => ProductModel.fromMap(json)).toList();
 
       return Right(products);
-    } on NetworkException catch(e) {
-      if(e.type != NetworkExceptionType.cancelled) {
+    } on NetworkException catch (e) {
+      if (e.type != NetworkExceptionType.cancelled) {
         GlobalSnackBar.showError(e.message);
       }
       return Left(e.toString());
